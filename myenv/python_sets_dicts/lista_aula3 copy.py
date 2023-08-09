5889ff1faf4f281e2fefeffd56c21987eb7faeb5
Mauricio Ribeiro <mauricioribeiro61@gmail.com>
2023-08-09 00:31:47 -0300

    Update lista_aula3 copy.py

--- myenv/python_sets_dicts/lista_aula3 copy.py
+++ myenv/python_sets_dicts/lista_aula3 copy.py
@@ -67,29 +67,47 @@ if iniciar == 'Y':'''
 #DONE
 from collections import defaultdict
 import json
+import itertools
 
 students = defaultdict(str)
 
 students['1']
 students['2']
 
-StdsInfo = [{'name': 'mauricio', 'grade1': '8'},{'name': 'maumau', 'grade1': '6'}]
+StdsInfo = [{'name': 'mauricio', 'grade1': '8', 'grade2': '6', 'grade3': '7', 'grade4': '2'},{'name': 'maumau', 'grade1': '6'}]
 nested_students = dict(zip(students, StdsInfo))
 
-def create_student(nested_students, std_nome, grade_1):
+def create_student(nested_students, std_nome):
   ultimo_estudante = list(nested_students)[-1]
   ultimo_estudante_n = int(ultimo_estudante) + 1
   variavel_to_str = str(ultimo_estudante_n)
-  nested_students[variavel_to_str] = {'name': std_nome, 'grade1': grade_1}
+  grade_1 = input('digite sua primeira nota')
+  grade_2 = input('digite sua segunda nota')
+  grade_3 = input('digite sua terceira nota')
+  grade_4 = input('digite sua quarta nota')
+  nested_students[variavel_to_str] = {'name': std_nome, 'grade1': grade_1, 'grade2': grade_2, 'grade3': grade_3, 'grade4': grade_4}
   nested_students = dict(zip(students, StdsInfo))
-create_student(nested_students, 'foo bar', 9)
-create_student(nested_students, 'lorem ipsum', 8.5)
-create_student(nested_students, 'foo bar1', 7.3)
+#create_student(nested_students, 'foo bar')
+#create_student(nested_students, 'lorem ipsum')
+#create_student(nested_students, 'foo bar1')
 
+
+#slice para iterar apenas entre as notas????  depois da um apend e cria uma array das notas do aluno
+#colocar condicional para nenhuma das notas ser zero, senao o programa vai bugar
+print(int(nested_students['1']['grade1'])+int(nested_students['1']['grade2'])+int(nested_students['1']['grade3'])+int(nested_students['1']['grade4']))
+
+grades_grouped = itertools.islice(nested_students['1'].values(), 1, 5)
+print(grades_grouped)
+print(type(grades_grouped))
+listado = list()
+
+for i in grades_grouped:
+  listado.append(int(i))
+print(sum(listado))
+
+#for i in range(0, 2)
 print(json.dumps(nested_students, indent=4))
   
-  
 
-   
 
 
