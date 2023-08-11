9478c6a8aa4d42994b4fae6c41757948dbad4b72
Mauricio Ribeiro <mauricioribeiro61@gmail.com>
2023-08-11 17:39:30 -0300

    refactoring code

--- myenv/python_funcoes/python_aula04_q3.py
+++ myenv/python_funcoes/python_aula04_q3.py
@@ -3,9 +3,12 @@ import string
 
 palavra_frase = 'm@uricio estuda python!'
 
-qtd = len(palavra_frase)
-#print(qtd)
+qtt = len(palavra_frase)
 
+only_letters = len(re.sub(pattern='\W', repl='', string=palavra_frase))
+
+#FIRST WAY
+#removing spaces with for loop
 def find_blank(palavra):
 #tem que sair de n=1 porque significa que ele ja achou o primeiro espaço, caso contrário irá contabilizar um a menos
   n=1
@@ -13,40 +16,35 @@ def find_blank(palavra):
    if(i.isspace()):
       n+=1
       return f'Através de método encontrando a quantiade de espaços é {n}'
-print(find_blank(palavra_frase))
+#print(find_blank(palavra_frase))
 
-#removendo os espaços
+#SECOND WAY
+#removing spaces with regex
 palavra_without_blank = re.sub(r'[\s+]', '', palavra_frase)
 
-#calculando a diferença 
-print(f'A frase escrita sem espaços fica dessa forma {palavra_without_blank}')
-qtd_spc = len(palavra_frase) - len(palavra_without_blank)
+qtt_spc = qtt - len(palavra_without_blank)
 
-#removendo as pontuações
+#FIRST WAY
+#removing punctuations with regex
 palavra_without_punc = re.sub(r'[^\w\s]', '', palavra_frase)
 
-#calculando a diferença 
-qtd_punc = len(palavra_frase) - len(palavra_without_punc)
-
-print(f'A quantidade de acentos é: {qtd_punc}')
-print(f'A quantidade de espaços é: {qtd_spc}')
+qtt_punc = qtt - len(palavra_without_punc)
    
-#using string import and iteration
+#SECOND WAY
+#using string import and iteration para remover as pontuações
 punctuations_list = string.punctuation
 
 acentos = []
 testing_string_punc = {letra for letra in palavra_frase}
-print(testing_string_punc)
 for letra in testing_string_punc:
   if letra in punctuations_list:
     acentos.append(letra)
-print(len(acentos))
 
 
-
-'''Estudar
+print(f'A quantidade de letras é: {only_letters}')
+print(f'A quantidade de acentos é: {qtt_punc}')
+print(f'A quantidade de espaços é: {qtt_spc}')
 
 #using regex
-print(re.sub(pattern='^\w\s', repl='$', string=palavra_frase))
+'''print(re.sub(pattern='^\w\s', repl='$', string=palavra_frase))'''
 
-print(re.sub(pattern='\W', repl='$', string=palavra_frase))'''
