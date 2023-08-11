8e922c0e09b6b8b5329852b99066011e367a658a
Mauricio Ribeiro <mauricioribeiro61@gmail.com>
2023-08-11 17:52:22 -0300

    defining method

--- myenv/python_funcoes/python_aula04_q3.py
+++ myenv/python_funcoes/python_aula04_q3.py
@@ -1,13 +1,10 @@
 import re
 import string
 
-palavra_frase = 'm@uricio estuda python!'
+palavra_frase = input('Digite uma palavra ou frase: ')
 
-qtt = len(palavra_frase)
-
-only_letters = len(re.sub(pattern='\W', repl='', string=palavra_frase))
-
-#FIRST WAY
+'''
+#SECOND WAY
 #removing spaces with for loop
 def find_blank(palavra):
 #tem que sair de n=1 porque significa que ele ja achou o primeiro espaço, caso contrário irá contabilizar um a menos
@@ -18,17 +15,6 @@ def find_blank(palavra):
       return f'Através de método encontrando a quantiade de espaços é {n}'
 #print(find_blank(palavra_frase))
 
-#SECOND WAY
-#removing spaces with regex
-palavra_without_blank = re.sub(r'[\s+]', '', palavra_frase)
-
-qtt_spc = qtt - len(palavra_without_blank)
-
-#FIRST WAY
-#removing punctuations with regex
-palavra_without_punc = re.sub(r'[^\w\s]', '', palavra_frase)
-
-qtt_punc = qtt - len(palavra_without_punc)
    
 #SECOND WAY
 #using string import and iteration para remover as pontuações
@@ -39,12 +25,19 @@ testing_string_punc = {letra for letra in palavra_frase}
 for letra in testing_string_punc:
   if letra in punctuations_list:
     acentos.append(letra)
-
-
-print(f'A quantidade de letras é: {only_letters}')
-print(f'A quantidade de acentos é: {qtt_punc}')
-print(f'A quantidade de espaços é: {qtt_spc}')
-
+'''
 #using regex
 '''print(re.sub(pattern='^\w\s', repl='$', string=palavra_frase))'''
 
+
+def refactor_sentence(palavra_do_usuario):
+  qtt = len(palavra_do_usuario)
+  only_letters = len(re.sub(pattern='\W', repl='', string=palavra_frase))
+  palavra_without_blank = re.sub(r'[\s+]', '', palavra_frase)
+  qtt_spc = qtt - len(palavra_without_blank)
+  palavra_without_punc = re.sub(r'[^\w\s]', '', palavra_frase)
+  qtt_punc = qtt - len(palavra_without_punc)
+  print(f'A quantidade de letras é: {only_letters}')
+  print(f'A quantidade de acentos é: {qtt_punc}')
+  print(f'A quantidade de espaços é: {qtt_spc}')
+refactor_sentence(palavra_frase)
